95ac6e5bb423996cfe59356e9f6b1f98a7bf3811
CTRL_Marcws <marfilipi@hotmail.com>
2021-03-08 14:53:59 -0300

    Segundo Commit

--- src/controller/ThreadVetor.java
+++ src/controller/ThreadVetor.java
@@ -2,47 +2,36 @@ package controller;
 
 public class ThreadVetor extends Thread
 {
-	
-	/*
-	 * Criar em java um projeto com uma Thread chamada ThreadVetor, que receba um valor numérico e
-	 * vetor como parâmetros. Caso o valor numérico seja par, a thread deve percorrer o vetor
-	 * utilizando uma estrutura for (int i = 0 ; i < vet.length; i++) e contar o tempo para
-	 * percorrer o vetor. Caso o valor numérico seja ímpar, a thread deve percorrer o vetor
-	 * utilizando uma estrutura foreach e contar o tempo para percorrer o vetor. No final,
-	 * ambas as possibilidades devem apresentar o tempo em segundos.
-	 * 
-	 * A operação main deve gerar um vetor de 1000 posições com valores aleatórios de 1 a 100.
-	 * Deve iniciar 2 ThreadVetor e para uma passar o número 1 e o vetor e para a outra, passar
-	 * o número 2 e o mesmo vetor.
-	 */
-	
-	/*
-	 * recebe vetor e int ok
-	 * "dentro do run" fazer a verificação
-	 */
+	//Declaração de Variaveis
 	private int x;
 	private int vet [] = new int [1000];
 	
+	//Método construtor
 	public ThreadVetor(int x, int vet [])
 	{
 		this.x = x;
 		this.vet = vet;
 	}
 	
+	//Método RUN chamando o método contador()
 	@Override
 	public void run()
 	{
 		contador();
 	}
 	
+	//Função para determinar se é par ou impar
 	public boolean ePar(int x)
 	{
-		if (x % 2 == 0) {			
+		if (x % 2 == 0)
+		{			
 			return true;
 		}
+		
 		return false;
 	}
 	
+	//Método contador, que percorre o vetor e retorna o tempo.
 	public void contador()
 	{
 		double tempoInicial, tempoFinal, total;
--- src/view/Main.java
+++ src/view/Main.java
@@ -9,15 +9,18 @@ public class Main
 
 	public static void main(String[] args)
 	{
+		//Declaração de variaveis
 		Random random = new Random();
 		int x;
 		int vet [] = new int [1000];
 
+		//Populando vetor com valores aleatorios de 1 a 100
 		for (int i = 0; i < vet.length; i++)
 		{
 			vet[i] = random.nextInt(101);
 		}
 
+		//Chamada das Threads enviando 1 e 2
 		for (x = 1; x < 3; x++)
 		{
 			Thread threadVetor = new ThreadVetor(x, vet);
